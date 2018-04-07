015b39ae7e1600c3d93d2030e0732f5aeb9f9219
Başar Kaya <kayaism@gmail.com>
2018-04-07 17:26:05 +0300

    Add project files.

---
+++ MvcAunhentication/Controllers/SecretController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAunhentication.Controllers
+{
+    [Authorize]
+    public class SecretController : Controller
+    {        
+        [AllowAnonymous]
+        public ContentResult Index()
+        {
+            #region old code
+            //StringBuilder message = new StringBuilder();
+
+            //message.AppendLine("This is a secret.");
+            //message.AppendLine("User is authenticated.");
+
+            //if (User.Identity.IsAuthenticated)//veya Request.IsAuthenticated
+            //{
+            //    message.AppendLine("This is a secret.");
+            //    message.AppendLine("User is authenticated.");
+            //}
+            //else
+            //{
+            //    message.AppendLine("User is not authenticated.");
+            //}
+
+            //return Content(message.ToString());
+
+            #endregion
+
+            return Content("This is not a secret");
+        }
+                
+        public ContentResult Secret()
+        {
+            StringBuilder message = new StringBuilder();
+
+            message.AppendLine("This is a secret.");
+            message.AppendLine("User is authenticated.");
+
+            return Content(message.ToString());
+        }
+    }
+}
---
+++ MvcAunhentication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcAunhentication.Startup))]
+namespace MvcAunhentication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
